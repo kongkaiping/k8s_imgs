51310f0d3a64ff18defe598292e160bff092bc39
kongkp1 <kongkp1@lenovo.com>
2019-02-16 05:56:05 +0000

    update version

--- pull_and_chang_tag.sh
+++ pull_and_chang_tag.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-images=(kube-apiserver:v1.3.3 kube-controller-manager:v1.3.3 kube-scheduler:v1.3.3 kube-proxy:v1.3.3
+images=(kube-apiserver:v1.13.3 kube-controller-manager:v1.13.3 kube-scheduler:v1.13.3 kube-proxy:v1.13.3
 pause:3.1 etcd:3.2.24 coredns:1.2.6)
 for image in ${images[@]} ; do
   docker pull kongkaiping/$image
